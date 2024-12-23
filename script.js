629ac62a16de9441f0607a6960423943c852f0f9
Shreyash-gupta-19 <shreyashgupta286@gmail.com>
2024-12-23 20:41:31 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var main = document.querySelector(".main")
-
-var cur = document.querySelector(".cursor")
-
-main.addEventListener("mousemove",function(dets){
-    cur.style.left = dets.x+"px"
-    cur.style.top = dets.y+"px"
-})

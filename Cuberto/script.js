12e2f16c440b65ac5c8de1e57b19eb6766a092c1
Shreyash-gupta-19 <shreyashgupta286@gmail.com>
2024-12-23 20:38:38 +0530

    Delete Cuberto directory

--- Cuberto/script.js
+++
@@ -1,3 +0,0 @@
-Shery.mouseFollower();
-Shery.makeMagnet(".magnet");
-Shery.hoverWithMediaCircle(".hvr",{videos: ["https://cuberto.com/assets/home/hero/header.mp4", "https://cuberto.com/assets/showreel/short.mp4"]});

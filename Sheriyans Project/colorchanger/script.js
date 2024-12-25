ba2bc2f1aad1230d64fdeeda906e81105aeb836f
Shreyash-gupta-19 <shreyashgupta286@gmail.com>
2024-12-25 23:31:58 +0530

    Delete Sheriyans Project directory

--- Sheriyans Project/colorchanger/script.js
+++
@@ -1,9 +0,0 @@
-var btns = document.querySelectorAll("button")
-var body = document.querySelector("body")
-
-btns.forEach(function(elem){
-    elem.addEventListener("click",function(){
-        body.style.backgroundColor = elem.innerHTML
-    })
-    
-})
